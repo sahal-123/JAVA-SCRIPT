848d30e784ffe647973499fbc3908ff8168f6946
sahal-123 <munnasmunna782@gmail.com>
2025-08-25 11:01:28 +0530

    25-08-2025

---
+++ NON-PRIMITIVE/aacessing-modyfing-arrey.js
@@ -0,0 +1,9 @@
+let colors = ["red","blue","green"]
+
+// accessing first Element
+let firstcolor = colors[0]
+console.log('first color: ',firstcolor);
+
+// modyfing second colors
+colors[1] = "yellow"
+console.log('modyfied colors:',colors)
